ccd88174a20c4d34d28b7748f3f83ddbb87aa8bb
Steve <steve@Steves-iMac.local>
2018-08-07 18:19:44 -0400

    philosophy template and styles

--- Bahl_Gaynor_rebuild/philosophy-template.php
+++ Bahl_Gaynor_rebuild/philosophy-template.php
@@ -13,48 +13,36 @@
 	</div>
 		
 				
-		<div class="wdy-div">
+		<div class="why-div">
 			<div class="col-xs-12 nopadding tag-cont">
 				<p class="tagline">WHY DIVIDENDS?</p>
-				<p class="tagline sub-tags">Reduce Uncertainty. Increase Income.</p>
+				<p class="tagline sub-tag">Reduce Uncertainty. Increase Income.</p>
 			</div>
-			<div class="col-xs-12 nopadding under-purpose">
-				<p><?php the_field('under_value'); ?></p>		
-			</div>
-		</div>
 
-		
-		<div class="our-culture">
-		<div class="col-xs-12 col-md-6 nopadding tag-cont">
-				<p class="tagline">Singularly<br/>Committed</br>&amp; Aligned</p>
-			</div>
-			<div class="col-xs-12 col-md-6 nopadding blue-curve-txt">
-				<h1>Our Culture</h1>
-				<?php the_field('client_value'); ?>			
-			</div>
-			<div class="col-xs-12 nopadding under-purpose">
-				<p><?php the_field('under_value'); ?></p>		
-			</div>
-			<div class="color-blocks">
+			<?php the_field('why_dividends'); ?>
+
 				<?php
 
 			// check if the repeater field has rows of data
-			if( have_rows('culture_blocks') ):
+			if( have_rows('icon_list') ):
 
 
 			 	// loop through the rows of data
-			    while ( have_rows('culture_blocks') ) : the_row();
+			    while ( have_rows('icon_list') ) : the_row();
 
 				// display a sub field value
-			        $background = get_sub_field('background_color');
-			        $header = get_sub_field('header');
-			        $copy = get_sub_field('copy');
+			        $icon = get_sub_field('icon');
+			        $title = get_sub_field('title');
+			        $list_copy = get_sub_field('list_copy');
 
 
 			        ?>
-			        	<div class="col-xs-12 col-md-6 col-lg-3 nopadding block" style="background-color: <?=$background;?>;">
-			        		<b><?=$header;?></b>
-			        		<p><?=$copy;?></p>
+			        	<div class="col-xs-12 nopadding icon-list">
+			        		<img src="<?=$icon;?>">
+			        		<div class="icon-list-right">
+				        		<h2><?=$title;?></h2>
+				        		<p><?=$list_copy;?></p>
+			        		</div>
 			        	</div>
 
 			        <?
@@ -68,20 +56,14 @@
 			endif;
 
 			?>
-			</div>
 		</div>
 
-		<div class="our-history">
-			<div class="col-xs-12 col-md-6 nopadding blue-curve-txt">
-				<h1>Our History</h1>
-				<?php the_field('under_history'); ?>			
-			</div>
-			<div class="col-xs-12 col-md-6 nopadding tag-cont">
-				<p class="tagline">ONE VISION,</p>
-				<p class="tagline orange tags">SINCE DAY</br>ONE.</p>
+		<div class="inv-com">
+			<div class="col-xs-12 nopadding tag-cont">
+				<p class="tagline">INVESTMENT COMMITTEE</p>
 			</div>
 			<div class="col-xs-12 nopadding under-purpose">
-				<p><?php the_field('history'); ?></p>		
+				<p><?php the_field('investment_committee'); ?></p>		
 			</div>
 		</div>
 
